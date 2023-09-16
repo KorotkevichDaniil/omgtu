6f4310e01cb927b60a7934812f1ec9bec6c5cab5
Daniil Korotkevich <dshbor@gmail.com>
2023-09-16 21:21:09 +0600

    16.09.2023

---
+++ Олимпиадные задачи/Грядки/Грядки/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] k = new int[] { 1, 2, 3, 20 };
+int l = 7;
+int n = 5;
+int m = 10;
+int sum = 0;
+
+for(int i = 0; i < k.Length; i++)
+{
+    for(int j = 0; j < k[i]; j++)
+    {
+        sum += 2 * l + 2 * (m + n) + m * j * 2;
+    }
+    Console.WriteLine("При k = " + k[i]);
+    Console.WriteLine("Путь равен " + sum);
+    sum = 0;
+}
+
+Console.ReadLine();
